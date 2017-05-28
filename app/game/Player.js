45720bac35c2547a228d59a8e247880da641d78f
joshlgrossman <josh.l.grossman@gmail.com>
2017-05-28 09:43:01 -0500

    fix friction bug

--- app/game/Player.js
+++ app/game/Player.js
@@ -6,22 +6,31 @@ class Player extends Listenable(Entity) {
   constructor(id){
     super();
     this.id = id;
+    this.speed = 100;
+    this.speedSq = this.speed**2;
+    this.friction = 0.85;
   }
 
   update(delta){
     if(this.keys[65]){
-      this.vel.x = -100;
+      this.vel.x = -this.speed;
     } else if(this.keys[68]){
-      this.vel.x = 100;
+      this.vel.x = this.speed;
     }
     if(this.keys[87]){
-      this.vel.y = -100;
+      this.vel.y = -this.speed;
     } else if(this.keys[83]){
-      this.vel.y = 100;
+      this.vel.y = this.speed;
+    }
+
+    if(this.vel.lengthSq > this.speedSq){
+      this.vel.length = this.speed;
     }
 
     this.pos = this.pos.add(this.vel.scale(delta));
-    this.vel = this.vel.scale(0.9);
+    let friction = 1 - delta;
+    if(friction < 0) friction = 0;
+    this.vel = this.vel.scale(this.friction * friction);
   }
 
   render(gfx){
