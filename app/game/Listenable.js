fe66190d4e6f86472573134f6c3874f97b6667c1
joshlgrossman <josh.l.grossman@gmail.com>
2017-05-27 19:44:01 -0500

    interface for listening for input

---
+++ app/game/Listenable.js
@@ -0,0 +1,27 @@
+const Event = require('./Event');
+
+const Listenable = Base => class extends Base {
+
+  constructor(...args){
+    super(...args);
+    this.keys = [];
+    this.mouse = {};
+  }
+
+  listen(io){
+    io.listeners.push(this);
+    return io;
+  }
+
+  remove(){}
+
+  on(evt, data){
+    if(evt === Event.MOUSE) this.mouse = data;
+    else if(evt === Event.KEY) this.keys[data.which] = data.down;
+    else if(evt === Event.DISCONNECT) this.remove();
+    return this;
+  }
+
+};
+
+module.exports = Listenable;
